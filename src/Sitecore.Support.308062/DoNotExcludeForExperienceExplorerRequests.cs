bd6def9d3c2cc92a4b64986784ea06fec894bf05
Sergey Plashenko <sergey.plashenko@sitecore.com>
2019-01-31 17:20:05 +0200

    Fix for the issue.

---
+++ src/Sitecore.Support.308062/DoNotExcludeForExperienceExplorerRequests.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Analytics.Pipelines.ExcludeRobots;
+
+namespace Sitecore.Support
+{
+  public class DoNotExcludeForExperienceExplorerRequests : ExcludeRobotsProcessor
+  {
+    public override void Process(ExcludeRobotsArgs args)
+    {
+      var mode = Context.Items["SC_ExperienceExplorer_Mode"] as bool?;
+      bool urlContainsExplorer = false;
+      if (Context.Request != null && Context.Request.FilePath != null)
+      {
+        urlContainsExplorer = Context.Request.FilePath.Contains("sitecore modules/web/experienceexplorer/") ||
+                              Context.Request.FilePath.Contains("sitecore/api/ssc/experienceexplorer/");
+      }
+
+      if (urlContainsExplorer || (mode.HasValue && mode.Value))
+      {
+        args.IsInExcludeList = false;
+        args.AbortPipeline();
+      }
+    }
+  }
+}
